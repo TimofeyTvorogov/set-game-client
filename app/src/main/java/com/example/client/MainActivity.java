fad9a5c9cd9d87c3a98ef28601252ccb6c86fa6c
colgate116 <shavrin1609@gmail.com>
2023-06-26 20:40:26 +0800

    recyclerView начат

--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -4,28 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.example.client.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Card> cardList;
+    ActivityMainBinding binding;
     private static final String address = "http://84.201.155.174";
     private Retrofit retrofit;
     private Api api;
-    private List<Card> cardList;
     private Button btn_FrLog;
     private ConstraintLayout constraintLayout;
     //change
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(address)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -34,24 +43,42 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Фрагмент входа
-        btn_FrLog = findViewById(R.id.btn_Log);
-        constraintLayout = findViewById(R.id.constraint_layout);
+        /*btn_FrLog = findViewById(R.id.btn_Log);
 
-        setLogFragment();
+        setLogFragment();*/
 
-        btn_FrLog.setOnClickListener(new View.OnClickListener() {
+        cardList = new ArrayList<>();
+        setCardInfo();
+        setAdapter();
+
+        /*btn_FrLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setLogFragment();
             }
-        });
+        });*/
     }
 
-    private void setLogFragment() {
+    private void setAdapter() {
+        recyclerAdapter adapter = new recyclerAdapter(cardList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        binding.recyclerView.setLayoutManager(layoutManager);
+        binding.recyclerView.setItemAnimator(new DefaultItemAnimator());
+        binding.recyclerView.setAdapter(adapter);
+    }
+
+    private void setCardInfo() {
+        cardList.add(new Card(1,1,1,1,1));
+        cardList.add(new Card(1,1,1,1,1));
+        cardList.add(new Card(1,1,1,1,1));
+
+    }
+
+    /*private void setLogFragment() {
         LogFragment logFragment = new LogFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.constraint_layout, logFragment);
 
-    }
+    }*/
 
 }
---
+++ app/src/main/java/com/example/client/recyclerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.client;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder>{
+    private ArrayList<Card> cardList;
+
+    public recyclerAdapter(ArrayList<Card> cardList){
+        this.cardList = cardList;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+        public MyViewHolder(final View view){
+            super(view);
+        }
+    }
+
+    @NonNull
+    @Override
+    public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardList.size();
+    }
+}
